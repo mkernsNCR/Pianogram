c7fe57fd6662581c09d87edfcd457f9bd683d626
Matthew Lamar Kerns <matthewkerns@ymail.com>
2016-03-21 11:40:41 -0400

    Configured video controller, model, and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: "videos#index"
+  resources :videos
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
